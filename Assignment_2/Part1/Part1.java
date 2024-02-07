b792f67b56d722a8e483bd8d0ce73784c78e8064
khushiarora0208 <159174169+khushiarora0208@users.noreply.github.com>
2024-02-07 18:14:48 +0530

    Delete Assignment_2/Part1/Part1.java

--- Assignment_2/Part1/Part1.java
+++
@@ -1,8 +0,0 @@
-public class Part1
-{
-    public static void main(String[] args)
-    {
-        EvenOdd eo = new EvenOdd(); // creating an instance of the EvenOdd class
-        eo.evenOdd(); // invoking the evenOdd method of the EvenOdd class
-    }
-}
